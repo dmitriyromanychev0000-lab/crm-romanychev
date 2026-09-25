90c123b62b40c4b510711ca0c2ea70c66d08aa4f
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-25 17:42:24 +0300

    chore: bump offline cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v3";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
+const CACHE = "crm-romanychev-v4";
+const ASSETS = ["./", "./index.html", "./styles.css?v=2", "./app.js?v=2", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
