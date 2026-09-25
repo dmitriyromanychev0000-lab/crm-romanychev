746fd80efc3311b8b1922dcb0083c93db8f6d8f4
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-26 00:47:24 +0300

    chore: bump cache to v60

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v59";
-const ASSETS = ["./", "./index.html", "./styles.css?v=33", "./app.js?v=55", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v60";
+const ASSETS = ["./", "./index.html", "./styles.css?v=34", "./app.js?v=56", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
