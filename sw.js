c54a747caba1c7cac4ea76da8560ca522d089ad4
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-25 20:01:44 +0300

    chore: refresh cache for warehouse screen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v31";
-const ASSETS = ["./", "./index.html", "./styles.css?v=11", "./app.js?v=27", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v32";
+const ASSETS = ["./", "./index.html", "./styles.css?v=12", "./app.js?v=28", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
