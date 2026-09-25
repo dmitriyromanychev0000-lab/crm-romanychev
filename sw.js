a428f6b66ff2b91d1a8add72c39de452f93416bf
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-25 19:12:39 +0300

    chore: refresh cache for mobile CSS

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v22";
-const ASSETS = ["./", "./index.html", "./styles.css?v=6", "./app.js?v=20", "./manifest.webmanifest"];
+const CACHE = "crm-romanychev-v23";
+const ASSETS = ["./", "./index.html", "./styles.css?v=7", "./app.js?v=20", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
