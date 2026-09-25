313a972eafc5d67689ecac49fa6f6c3614d9a684
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-25 20:48:01 +0300

    chore: refresh cache for goods and act

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v38";
-const ASSETS = ["./", "./index.html", "./styles.css?v=18", "./app.js?v=34", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v39";
+const ASSETS = ["./", "./index.html", "./styles.css?v=19", "./app.js?v=35", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
