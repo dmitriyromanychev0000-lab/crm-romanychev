6d9fa925ab63449253fd0f65ddc9a5ead3290ebd
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-25 19:05:44 +0300

    chore: bump app version to 0.19.0

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const DB_VERSION = 1;
 const STORE = "keyval";
 const DATA_KEY = "crm-data";
 const DIRECTORY_KEY = "backup-directory";
-const APP_VERSION = "0.18.0";
-const APP_BUILD = "2026.09.25.18";
+const APP_VERSION = "0.19.0";
+const APP_BUILD = "2026.09.25.19";
 const APP_URL = "https://dmitriyromanychev0000-lab.github.io/crm-romanychev/";
 
 const defaultData = () => ({
