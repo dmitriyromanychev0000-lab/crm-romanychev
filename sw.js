4bebc7f926d5beeb7f581f0f63b90991290fc449
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-25 21:09:34 +0300

    chore: bump service worker cache to v46

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v45";
-const ASSETS = ["./", "./index.html", "./styles.css?v=25", "./app.js?v=41", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v46";
+const ASSETS = ["./", "./index.html", "./styles.css?v=25", "./app.js?v=42", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
