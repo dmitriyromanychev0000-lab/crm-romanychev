d98077ce4511619bfd7f0772f93acf493908974d
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-25 19:51:06 +0300

    chore: refresh cache for visual step one

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v29";
-const ASSETS = ["./", "./index.html", "./styles.css?v=9", "./app.js?v=25", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v30";
+const ASSETS = ["./", "./index.html", "./styles.css?v=10", "./app.js?v=26", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
