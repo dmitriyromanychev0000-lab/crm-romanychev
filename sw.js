e64868fe64fe1e5aa4857d97907f0f5c84f8343e
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-25 17:40:50 +0300

    chore: refresh offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "crm-romanychev-v2";
+const CACHE = "crm-romanychev-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
