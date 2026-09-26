b670605a30dc699713f99aa0de37586c2d633805
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-26 18:38:24 +0300

    Refresh cache for price list rebuild

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v109";
-const ASSETS = ["./", "./index.html", "./styles.css?v=72", "./app.js?v=98", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v110";
+const ASSETS = ["./", "./index.html", "./styles.css?v=73", "./app.js?v=99", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
