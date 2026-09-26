c49f5a6c47f35f77007fb358c76d635d9b79fbc7
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-26 18:00:08 +0300

    Bump cache after visual integration sweep

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v102";
-const ASSETS = ["./", "./index.html", "./styles.css?v=66", "./app.js?v=91", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v103";
+const ASSETS = ["./", "./index.html", "./styles.css?v=66", "./app.js?v=92", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
