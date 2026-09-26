6a2b5e82007212ebcba63f888070c1b23ddd9446
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-26 21:34:00 +0300

    Refresh cache for mobile scale pass

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v132";
-const ASSETS = ["./", "./index.html", "./styles.css?v=95", "./mobile-v2.css?v=1", "./app.js?v=119", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v133";
+const ASSETS = ["./", "./index.html", "./styles.css?v=95", "./mobile-v2.css?v=2", "./app.js?v=120", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
