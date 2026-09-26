77330f40f8c411f27a90252645284684b3f56e0b
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-26 17:16:13 +0300

    Bump cache after Sheet 02 completion

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v92";
-const ASSETS = ["./", "./index.html", "./styles.css?v=56", "./app.js?v=81", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v93";
+const ASSETS = ["./", "./index.html", "./styles.css?v=57", "./app.js?v=82", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
