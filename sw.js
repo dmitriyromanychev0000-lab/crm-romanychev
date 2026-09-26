97c28ad2996a7738a04e69002e26063a6868d820
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-26 18:46:19 +0300

    Refresh cache for clients and finance rebuild

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v111";
-const ASSETS = ["./", "./index.html", "./styles.css?v=74", "./app.js?v=100", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v112";
+const ASSETS = ["./", "./index.html", "./styles.css?v=75", "./app.js?v=101", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
