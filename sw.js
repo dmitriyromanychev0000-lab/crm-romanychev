710c74b29bc869035010d9e15661eae04626fef9
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-26 19:50:36 +0300

    Refresh cache for mobile sheet rebuild

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v117";
-const ASSETS = ["./", "./index.html", "./styles.css?v=80", "./app.js?v=106", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v118";
+const ASSETS = ["./", "./index.html", "./styles.css?v=81", "./app.js?v=107", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
