6bdad969243748100c2810f6f7c64a7046b9cf7b
dmitriyromanychev0000-lab <dmitriy.romanychev0000@gmail.com>
2026-09-26 21:14:35 +0300

    Refresh cache for 0.86 redesign

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "crm-romanychev-v127";
-const ASSETS = ["./", "./index.html", "./styles.css?v=90", "./app.js?v=116", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "crm-romanychev-v128";
+const ASSETS = ["./", "./index.html", "./styles.css?v=91", "./app.js?v=117", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
